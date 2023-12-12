79d4fd84e87da0550efd0dc662ee361d2912885e
Brandon Yang <107154811+branyang02@users.noreply.github.com>
2023-12-11 21:12:49 -0500

    updated python to 3.9

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "Building Project..."
-python -m pip install -r requirements.txt
+python3.9 -m pip install -r requirements.txt
 
 echo "Make migrations..."
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
 
 echo "Collect static files..."
-python manage.py collectstatic --noinput --clear
+python3.9 manage.py collectstatic --noinput --clear
